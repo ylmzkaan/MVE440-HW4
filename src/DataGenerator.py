52c356d4f4caf4acca3cfcfc9637f68819a2dd0b
Kaan Yilmaz <kaan-yilmaz@outlook.com.tr>
2019-05-15 12:20:31 +0200

    changed data generation to single cluster data generation

--- src/DataGenerator.py
+++ src/DataGenerator.py
@@ -1,56 +1,26 @@
 import numpy as np
 from Initialization import initClasses
 
-def generateData(numberOfClasses, numberOfFeatures, numberOfRecordsPerClass,
-                 featureMeanRange, randomNumberSeed, trueNumberOfClasses=None):
+def generateOneClusterData(numberOfFeatures, numberOfRecordsPerClass,
+                 featureMeanRange, randomNumberSeed, distribution="normal"):
+    
+    featureDistributionData = initClasses(1, numberOfFeatures,
+                                        featureMeanRange,
+                                        randomNumberSeed=randomNumberSeed)[0]
 
-    if trueNumberOfClasses == None:
-        trueNumberOfClasses = numberOfClasses
-
-    TOTAL_NUMBER_OF_RECORDS = numberOfClasses * numberOfRecordsPerClass
-    
-    featureDistributionData = initClasses(trueNumberOfClasses, numberOfFeatures,
-                                            featureMeanRange,
-                                            randomNumberSeed=randomNumberSeed)
-    
-    if trueNumberOfClasses == None:
-        classIdx = list(range(numberOfClasses))
-    else:
-        classIdx = np.random.choice(trueNumberOfClasses, numberOfClasses)
-    # Initialize data nxp
-    data = np.empty((TOTAL_NUMBER_OF_RECORDS, numberOfFeatures))
-    for i in range(numberOfClasses):
-        idx = classIdx[i]
-        generatedFeatures = np.random.multivariate_normal(featureDistributionData[idx].featureMeans,
-                                                        featureDistributionData[idx].featureCovariances,
-                                                        size=(numberOfRecordsPerClass))
-
-        rowRangeForCurrentClass = range(i*numberOfRecordsPerClass,(i+1)*numberOfRecordsPerClass)                                                        
-        data[rowRangeForCurrentClass,:] = generatedFeatures
-    
-    labels = list(range(numberOfClasses))
-    labels = np.repeat(labels, numberOfRecordsPerClass)
-    
-    #Shuffle with labels
-    concatenatedData = np.concatenate((data, labels[:,np.newaxis]), axis=1)
-    np.random.shuffle(concatenatedData)
-    
-    data = concatenatedData[:,:-1]
-    labels = concatenatedData[:,-1]
-    
-    return (data, labels)
-
-if __name__ == "__main__":
-    
-    NUMBER_OF_CLASSES = 4
-    NUMBER_OF_FEATURES = NUMBER_OF_CLASSES*2
-    NUMBER_OF_FEATURES_PER_CLASS = 200
-    TOTAL_NUMBER_OF_RECORDS = NUMBER_OF_CLASSES * NUMBER_OF_FEATURES_PER_CLASS
+    if distribution == "uniform":
+        data = np.random.uniform(featureDistributionData.featureMeans,
+                                featureDistributionData.featureMeans + 
+                                featureDistributionData.featureCovariances,
+                                size=(numberOfRecordsPerClass))
+    elif distribution == "normal":
+        data = np.random.multivariate_normal(featureDistributionData.featureMeans,
+                                            featureDistributionData.featureCovariances,
+                                            size=(numberOfRecordsPerClass))
+    else:      
+        raise Exception("Distribution: " + distribution + "not implemented!")  
         
-    FEATURE_MEAN_RANGE = [0, 50]
+    np.random.shuffle(data)
         
-    RANDOM_NUMBER_SEED = 0
+    return data
     
-    data, labels = generateData(NUMBER_OF_CLASSES, NUMBER_OF_FEATURES,
-                                NUMBER_OF_FEATURES_PER_CLASS, FEATURE_MEAN_RANGE,
-                                RANDOM_NUMBER_SEED)
--- src/MDS-Trial.py
+++ src/MDS-Trial.py
@@ -1,45 +1,36 @@
 from matplotlib import pyplot as plt
 from sklearn.manifold import MDS
 import numpy as np
-from DataGenerator import generateData
-from Settings import (DEFAULT_NUMBER_OF_CLASSES,
-                        DEFAULT_NUMBER_OF_FEATURES,
+from DataGenerator import generateOneClusterData
+from Settings import (DEFAULT_NUMBER_OF_FEATURES,
                         DEFAULT_NUMBER_OF_RECORDS_PER_CLASS,
                         DEFAULT_FEATURE_MEAN_RANGE,
-                        DEFAULT_RANDOM_NUMBER_SEED,
-                        DEFAULT_TRUE_NUMBER_OF_CLASSES)
+                        DEFAULT_RANDOM_NUMBER_SEED)
 
-data, labels = generateData(DEFAULT_NUMBER_OF_CLASSES, 
-                            DEFAULT_NUMBER_OF_FEATURES,
+data = generateOneClusterData(DEFAULT_NUMBER_OF_FEATURES,
                             DEFAULT_NUMBER_OF_RECORDS_PER_CLASS,
                             DEFAULT_FEATURE_MEAN_RANGE, 
-                            DEFAULT_RANDOM_NUMBER_SEED,
-                            DEFAULT_TRUE_NUMBER_OF_CLASSES)
+                            DEFAULT_RANDOM_NUMBER_SEED)
 
 mds = MDS(n_components=2, n_jobs=-1)
 mdsData = mds.fit_transform(data)
 
-distinctTrainLabels = np.unique(labels)
-classColors = [np.random.rand(1,3) for _ in distinctTrainLabels]
+color = np.random.rand(1,3)
 OPACITY = 0.7
 
 plt.subplot(1, 2, 1)
 plt.title("Data Set With MDS")
 plt.xlabel("MDS Feature 1")
 plt.ylabel("MDS Feature 2")
-for i, label in enumerate(distinctTrainLabels):
-    plt.scatter(mdsData[labels==label,0], mdsData[labels==label,1],
-                c=classColors[i], alpha=OPACITY,
-                label="Class {}".format(i))
+plt.scatter(mdsData[:,0], mdsData[:,1],
+            c=color, alpha=OPACITY)
 plt.legend()  
 
 plt.subplot(1, 2, 2)
 plt.title("Data Set")
 plt.xlabel("Feature 1")
 plt.ylabel("Feature 2")
-for i, label in enumerate(distinctTrainLabels):
-    plt.scatter(data[labels==label,0], data[labels==label,1],
-                c=classColors[i], alpha=OPACITY,
-                label="Class {}".format(i))
+plt.scatter(data[:,0], data[:,1],
+            c=color, alpha=OPACITY)
 plt.legend()  
 plt.show()                
